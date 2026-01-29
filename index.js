437f6b878ebe064886880837347351ff156f3a79
Jakob Moritz <jakob.moritz.3@gmail.com>
2026-01-29 09:23:55 +0100

    Added a /stats site for IP-addresses of visitors

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const geoip = require('geoip-lite');
 
 const app = express();
 const PORT = 3001;
 
+let visitors = []; // Simple in-memory storage
+
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+    // Get the IP from Nginx header
+    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    
+    // geoip-lite doesn't like IPv6 loopback (::1) or local IPs
+    const geo = geoip.lookup(ip);
+
+    if (geo) {
+        visitors.push({
+            ip: ip,
+            city: geo.city,
+            country: geo.country,
+            region: geo.region,
+            ll: geo.ll, // Latitude and Longitude
+            time: new Date().toLocaleString()
+        });
+    }
+    
+    // Keep only the last 50 visitors to save memory
+    if (visitors.length > 50) visitors.shift();
+    
+    next();
+});
+
+app.get('/stats', (req, res) => {
+    let html = '<h1>Recent Visitors</h1><ul>';
+    visitors.forEach(v => {
+        html += `<li><strong>${v.time}</strong>: ${v.city}, ${v.country} (IP: ${v.ip})</li>`;
+    });
+    html += '</ul>';
+    res.send(html);
+});
 
 app.listen(PORT, () => {
   console.log(`Running at http://localhost:${PORT}`);
